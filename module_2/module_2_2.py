28fe8fef90fa9005bf9a4c5800ffb66543d3f4d2
AveCraig <AveCraig@gmail.com>
2024-09-01 21:59:09 +0700

    2/2 #2

--- module_2/module_2_2.py
+++ module_2/module_2_2.py
@@ -1,23 +1,11 @@
-first = 1
-second = 2
-third = 3
-if first == second and second == third:
-    print(3)
-elif first == second or second == third or first:
-    print(2)
-elif first != second and second != third and first != third:
-    print(0)
-else:
-    print("ololo попячся")
-
-first = input(int("Введите число №1: "))
-second = input(int("Введите число №2: "))
-third = input(int("Введите число №3: "))
+first = int(input("Введите число №1: "))
+second = int(input("Введите число №2: "))
+third = int(input("Введите число №3: "))
 
 
 if first == second and second == third:
     print(3)
-elif first == second or second == third or first:
+elif first == second or second == third or first == third:
     print(2)
 elif first != second and second != third and first != third:
     print(0)
